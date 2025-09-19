0ba65ca760abd812274b78729299e02a2f4deca7
Arinaldo-souza <naldinhosouza55455@gmail.com>
2025-09-18 21:21:03 -0300

    primeiros componentes

---
+++ src/components/itemaluguel/ItemAluguel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemAluguel = () => {
+    return (
+        <div>
+            <h1>ItemAluguel</h1>
+        </div>
+    );
+};
+
+export default ItemAluguel;
---
+++ src/pages/Aluguel.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from '../components/Menu/Menu';
+import ItemAluguel from '../components/ItemAluguel/ItemAluguel';
+
+const Aluguel = () => {
+    return (
+        <div>
+            <Menu />
+            <h1>Aluguel Page</h1>
+            <ItemAluguel />
+             <ItemAluguel />
+        </div>
+    );
+};
+
+export default Aluguel;
